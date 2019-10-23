a3ee77cf5e6b6c9642ed526de14d032fb5e1c525
Pablo <pablo@intvs.org>
2019-10-23 17:08:11 +0100

    remove comments

--- src/app/withdrawMoney/withdrawMoney.js
+++ src/app/withdrawMoney/withdrawMoney.js
@@ -2,43 +2,36 @@ import getAmountToWithdraw from '../getAmountToWithdraw/getAmountToWitdraw'
 
 function withdrawMoney(amountToWithdraw, notesAvailable, currentBalance) {
   const notesToWithdraw = {};
-  const statusOps = {
+  const resultOps = {
     notesAvailable,
     notesToWithdraw
   }
   // ATM Money
-  let amountNotesAvailable = getAmountToWithdraw(statusOps.notesAvailable);
-  // console.log('amountNotesAvailable: ' + amountNotesAvailable)  
+  let amountNotesAvailable = getAmountToWithdraw(resultOps.notesAvailable);
   // User Money
-  let amountAbleToWithdraw = currentBalance;
 
   if (amountToWithdraw > amountNotesAvailable) {
     amountToWithdraw = amountNotesAvailable;
   }
 
   while (amountToWithdraw !== 0 && amountToWithdraw > 0) {
-    console.log('amountWithdrawing: ' + amountToWithdraw);
-    for (let key2 in statusOps.notesAvailable) {
-      if ((amountToWithdraw - +key2) >= 0 && statusOps.notesAvailable[key2] > 0) {
+    for (let key2 in resultOps.notesAvailable) {
+      if ((amountToWithdraw - +key2) >= 0 && resultOps.notesAvailable[key2] > 0) {
         // console.log(amountToWithdraw);
         amountToWithdraw -= +key2;
-        if (typeof statusOps.notesToWithdraw[key2] === 'undefined') {
-          statusOps.notesToWithdraw[key2] = 1;
-          statusOps.notesAvailable[key2]--;
+        if (typeof resultOps.notesToWithdraw[key2] === 'undefined') {
+          resultOps.notesToWithdraw[key2] = 1;
+          resultOps.notesAvailable[key2]--;
         } else {
-          statusOps.notesToWithdraw[key2]++;
-          statusOps.notesAvailable[key2]--;
+          resultOps.notesToWithdraw[key2]++;
+          resultOps.notesAvailable[key2]--;
         }
       }
     }
   }
-  amountNotesAvailable = getAmountToWithdraw(statusOps.notesAvailable);
-  console.log('amountNotesAvailable: ' + amountNotesAvailable)
-  console.log(statusOps.notesAvailable)
-  // amountAbleToWithdraw -= amountToWithdraw;
-  console.log('amountAbleToWithdraw: ' + amountAbleToWithdraw);
+  amountNotesAvailable = getAmountToWithdraw(resultOps.notesAvailable);
 
-  return statusOps
+  return resultOps
 }
 
 export default withdrawMoney;
