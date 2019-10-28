967d322c08a6f6d8008c2c93cd30a97ac417084c
Pablo <pablo@intvs.org>
2019-10-28 19:29:09 +0000

    Update webpack config files

--- webpack.common.js
+++ webpack.common.js
@@ -1,33 +1,13 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
   module: {
     rules: [{
         test: /\.js$/,
         exclude: /node_modules/,
         loader: [{
           loader: 'babel-loader'
-        }]
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-        exclude: /\.module\.css$/
-      },
-      {
-        test: /\.png$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            mimetype: 'image/png'
-          }
         }]
       },
       {
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,17 +1,26 @@
 const path = require('path');
 const common = require('./webpack.common')
 const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: "development",
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
-  },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     hot: true,
     port: 9000
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+      exclude: /\.module\.css$/
+    }, ]
+  }
 });
